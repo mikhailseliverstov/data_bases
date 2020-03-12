5489304c2e29ce7589049a6e6ed9e37ef8d3b223
Mike Seliverstov <seliverstovmike@gmail.com>
2020-03-12 19:37:32 +0300

    lesson07upd

--- lesson07/lesson07.sql
+++ lesson07/lesson07.sql
@@ -1,7 +1,57 @@
 -- Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
-
+SELECT DISTINCT 
+	o.user_id, 
+	u.name 
+FROM orders o 
+JOIN users u ON o.user_id = u.id;
 
 -- Выведите список товаров products и разделов catalogs, который соответствует товару.
+SELECT 
+	p.id, 
+	p.name AS product_name, 
+	catalog_id, 
+	c.name AS catalog_name  
+FROM products p 
+JOIN catalogs c ON p.catalog_id = c.id;
 
 
--- (по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). Поля from, to и label содержат английские названия городов, поле name — русское. Выведите список рейсов flights с русскими названиями городов.
+-- Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). 
+-- Поля from, to и label содержат английские названия городов, поле name — русское. 
+-- Выведите список рейсов flights с русскими названиями городов.
+
+-- Создаём таблицы.
+DROP TABLE IF EXISTS flights;
+CREATE TABLE flights (
+  id SERIAL PRIMARY KEY,
+  from_city VARCHAR(255),
+  to_city VARCHAR(255)
+);
+	 
+INSERT INTO flights (from_city, to_city) VALUES
+	('moscow', 'omsk'),
+	('novgorod', 'kazan'),
+	('irkutsk', 'moscow'),
+	('omsk', 'irkutsk'),
+	('moscow', 'kazan');
+
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities (
+	label VARCHAR(255),
+	name VARCHAR(255)
+);
+	
+INSERT INTO cities (label, name) VALUES
+	('moscow', 'Москва'),
+	('omsk', 'Омск'),
+	('kazan', 'Казань'),
+	('irkutsk', 'Иркутск'),
+	('novgorod', 'Новгород');
+
+SELECT 
+	f.id, 
+	c.name, 
+	c1.name 
+FROM flights f 
+LEFT JOIN cities c ON from_city = c.label 
+LEFT JOIN cities c1 ON to_city = c1.label 
+ORDER BY id;
