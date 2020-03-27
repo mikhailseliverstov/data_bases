66b1593bb69534838b625b2a028d23e7caa65e9c
Mike Seliverstov <seliverstovmike@gmail.com>
2020-03-27 18:43:47 +0300

    lesson09

--- lesson09/lesson09.sql
+++ lesson09/lesson09.sql
@@ -51,20 +51,64 @@ GRANT SELECT ON *.view2 TO 'user_read'@'localhost' IDENTIFIED WITH sha256_passwo
 -- С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу "Добрый день",
 -- с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
 
+DELIMITER //
+CREATE FUNCTION hello ()
+RETURNS TEXT NOT DETERMINISTIC
+BEGIN
+	SELECT 
+		CASE
+			WHEN CURRENT_TIME BETWEEN '6:00:00' AND '12:00:00' THEN 'Good morning'
+			WHEN CURRENT_TIME BETWEEN '12:00:00' AND '18:00:00' THEN 'Good day'
+			WHEN CURRENT_TIME BETWEEN '18:00:00' AND '00:00:00' THEN 'Good evening'
+			WHEN CURRENT_TIME BETWEEN '00:00:00' AND '6:00:00' THEN 'Good night'
+		END;
+END //
+
 
 
 -- В таблице products есть два текстовых поля: name с названием товара и description с его описанием.
 -- Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема.
 -- Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. При попытке присвоить полям NULL-значение
 -- необходимо отменить операцию.
+USE shop;
+CREATE TRIGGER check_product_fields BEFORE INSERT ON poducts
+FOR EACH ROW 
+BEGIN 
+	DECLARE name1 TINYTEXT DEFAULT 'Name';
+	IF NEW.name = NULL AND NEW.description = NULL THEN 
+	SET name = name1
+	END IF;
+END//
 
+CREATE TRIGGER check_product_fields BEFORE UPDATE ON poducts
+FOR EACH ROW 
+BEGIN 
+	IF NEW.name = NULL AND NEW.description = NULL THEN 
+		SET NEW.name = OLD.name
+		SET NEW.description = OLD.description
+	END IF;
+END//
 
 
 -- Напишите хранимую функцию для вычисления произвольного числа Фибоначчи. Числами Фибоначчи называется последовательность в которой
 -- число равно сумме двух предыдущих чисел. Вызов функции FIBONACCI(10) должен возвращать число 55.
 
 
-
+CREATE FUNCTION FIBONACCI (value INT)
+RETURNS INT
+BEGIN
+	SET @x = 1;
+	DECLARE @n INT DEFAULT 1;
+	DECLARE @n1 INT DEFAULT 0;
+	DECLARE @f INT DEFAULT 0;
+	WHILE @x < value DO
+		SET @f = @n + @n1;
+		SET @n1 = @n;
+		SET @n = @f;
+		SET @x = @x + 1;
+	END WHILE;
+	RETURN @f;
+END//
 
 
 
